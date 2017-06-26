6b5c99a392aa3fcad30ecd01d80de9b5a1dfba48
Giridhar <giridharv38@gmail.com>
2017-06-26 13:58:53 -0700

    added greeting message to user

--- server/server.js
+++ server/server.js
@@ -11,7 +11,15 @@ app.use(express.static(publicPath))
 
 io.on('connection',function (socket) {
     console.log("New client connected")
-
+socket.emit('newMessage',{
+    from:'admin',
+    text:'welcome to the chat app',
+})
+socket.broadcast.emit('newMessage',{
+    from:'admin',
+    text:'new user joined',
+    createdAt:new Date().getTime()
+})
 socket.on('disconnect',function()
 {
     console.log("Client disconnected")
@@ -23,6 +31,12 @@ socket.on('createMessage',function (email) {
         text:email.text,
         createdAt: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage',
+    //     {
+    //         from:email.from,
+    //         text:email.text,
+    //         createdAt: new Date().getTime()
+    //     })
     })
 })
 
