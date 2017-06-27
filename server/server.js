8b2e58c85357fca53593225afce57b1a217862ba
Giridhar <giridharv38@gmail.com>
2017-06-26 19:05:09 -0700

    Added Timestamp

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var express= require('express')
 var http =require('http')
+var moment =require('moment')
 var socketIO = require('socket.io')
 var app = express()
 var server = http.createServer(app)
@@ -12,25 +13,34 @@ app.use(express.static(publicPath))
 io.on('connection',function (socket) {
     console.log("New client connected")
 socket.emit('newMessage',{
-    from:'admin',
+    from:'Admin',
     text:'welcome to the chat app',
 })
+socket.on('createLocation',function (res) {
+    io.emit('newLocation',{
+        from:'Admin',
+        url:'https://www.google.com/maps?q='+res.lat+','+res.lng,
+        createdAt: moment.valueOf()
+    })
+})
+
 socket.broadcast.emit('newMessage',{
-    from:'admin',
+    from:'Admin',
     text:'new user joined',
-    createdAt:new Date().getTime()
+    createdAt: moment.valueOf()
 })
 socket.on('disconnect',function()
 {
     console.log("Client disconnected")
 })
-socket.on('createMessage',function (email) {
+socket.on('createMessage',function (email,callback) {
     console.log(email)
     io.emit('newMessage',{
         from:email.from,
         text:email.text,
-        createdAt: new Date().getTime()
+        createdAt: moment.valueOf()
     })
+    callback()
     // socket.broadcast.emit('newMessage',
     //     {
     //         from:email.from,
